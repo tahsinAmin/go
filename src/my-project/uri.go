c329a87ee6f1066ac85d17b20028e589be0d4808
Muhammad Tahsin Amin <muhammadtahsinamin@gmail.com>
2022-01-27 19:22:11 +0600

    Office Update 01.27

--- src/my-project/uri.go
+++ src/my-project/uri.go
@@ -1,58 +1,75 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
-	// reader := bufio.NewReader(os.Stdin)
-	// Println := fmt.Println
-	var (
-	// lines             []string
-	// no_of_people      int
-	// questions         int
-	// no_one_solved_all bool
-	)
-
-	result := make([][]string, 4)
-	fmt.Printf("%q, %q, %q, %q \n", result[0][0], result[0][1], result[0][2], result[0][3])
-
-	fmt.Scan(&result[0][0], &result[0][1])
-
-	// // problems_solved := make([]int, questions)
-	// all := make([][]string, questions)
-	// cnt_solved := 0
-	// score := 0
-	// // Println(problems_solved[0])
-
-	// for x := 0; x < no_of_people; x++ {
-	// 	fmt.Scan(&all[x][0], &all[x][1], &all[x][2])
-
-	// 	// text, _ := reader.ReadString('\n')
-	// 	// text = strings.Replace(text, "\n", "", -1)
-	// 	// no_one_solved_all = strings.Contains(text, "0")
-	// 	// lines = strings.Split(text, " ")
-	// 	// all = append(all, lines)
-	// 	// for x := 0; x < len(lines); x++ {
-	// 	// 	if lines[x] == "1" {
-	// 	// 		problems_solved[x] = 1
-	// 	// 		cnt_solved += 1
-	// 	// 	}
-	// 	// 	if cnt_solved == questions {
-	// 	// 		break
-	// 	// 	}
-	// 	// }
+	// for i := 0; i < 2; i++ {
+	// 	const first = 2
+	// 	const sec = 3
+	// 	var twoD [first][sec]int
+	// 	for i := 0; i < 2; i++ {
+	// 		for j := 0; j < 3; j++ {
+	// 			twoD[i][j] = i + j
+	// 		}
+	// 	}
+	// 	fmt.Println("2d: ", twoD)
 	// }
 
-	// if no_one_solved_all {
-	// 	score += 1
-	// }
-	// if cnt_solved == questions {
-	// 	score += 1
-	// }
-
-	// Println(score)
-	// Println(all)
+	
 }
+
+// func main() {
+// 	// reader := bufio.NewReader(os.Stdin)
+// 	// Println := fmt.Println
+// 	var (
+// 	// lines             []string
+// 	// no_of_people      int
+// 	// questions         int
+// 	// no_one_solved_all bool
+// 	)
+
+// 	// result := make([][]int, 4)
+// 	// fmt.Printf("%d, %d, %d, %d \n", result[0], result[1], result[2], result[3])
+
+// 	result := make([][]int, 4)
+// 	fmt.Println(result)
+// 	// fmt.Printf("%d, %d, %d, %d \n", result[0][0], result[0][1], result[0][2], result[0][3])
+// 	// result = append(result, [1]int)
+
+// 	// fmt.Scan(&result[0][0], &result[0][1])
+
+// 	// // problems_solved := make([]int, questions)
+// 	// all := make([][]string, questions)
+// 	// cnt_solved := 0
+// 	// score := 0
+// 	// // Println(problems_solved[0])
+
+// 	// for x := 0; x < no_of_people; x++ {
+// 	// 	fmt.Scan(&all[x][0], &all[x][1], &all[x][2])
+
+// 	// 	// text, _ := reader.ReadString('\n')
+// 	// 	// text = strings.Replace(text, "\n", "", -1)
+// 	// 	// no_one_solved_all = strings.Contains(text, "0")
+// 	// 	// lines = strings.Split(text, " ")
+// 	// 	// all = append(all, lines)
+// 	// 	// for x := 0; x < len(lines); x++ {
+// 	// 	// 	if lines[x] == "1" {
+// 	// 	// 		problems_solved[x] = 1
+// 	// 	// 		cnt_solved += 1
+// 	// 	// 	}
+// 	// 	// 	if cnt_solved == questions {
+// 	// 	// 		break
+// 	// 	// 	}
+// 	// 	// }
+// 	// }
+
+// 	// if no_one_solved_all {
+// 	// 	score += 1
+// 	// }
+// 	// if cnt_solved == questions {
+// 	// 	score += 1
+// 	// }
+
+// 	// Println(score)
+// 	// Println(all)
+// }
 
 // https://www.beecrowd.com.br/judge/en/problems/view/1514
--- src/my-project/uri.py
+++ src/my-project/uri.py
@@ -4,75 +4,21 @@ while True:
     if everyone == problem == 0:
         break
 
+    characteristics = 4
     arr = []
-    nobody_solved_all = True
-    cnt_every_problem_solved_by_at_least_one = 0
-    no_problem_solved_by_everyone = True
-    cnt_if_solved_by_everyone = 0
-    everyone_solved_atleast_one = True
 
     for i in range(everyone):
         arr.append([int(j) for j in input().split()])
-        everyone_solved_atleast_one = False if arr[i].count(1) == 0 else True
-        nobody_solved_all = False if arr[i].count(0) == 0 else True
+        characteristics -= (1 if (arr[i].count(1) == 0) else 0)
+        characteristics -= (1 if (arr[i].count(0) == 0) else 0)
 
+    problemsAnswered = [0]*problem
     for i in range(problem):
-        is_every_problem_solved_by_at_least_one = False
-        if no_problem_solved_by_everyone:
-            no_problem_solved_by_everyone = True
-        cnt_if_solved_by_everyone = 0
         for j in range(everyone):
-            if arr[j][i] == 1:
+            problemsAnswered[j] += 1 if arr[j][i] == 1 else 0
 
-                if no_problem_solved_by_everyone:
-                    cnt_if_solved_by_everyone += 1
-                    if cnt_if_solved_by_everyone == everyone:
-                        no_problem_solved_by_everyone = False
+    for i in problemsAnswered:
+        characteristics -= 1 if i == 0 else 0
+        characteristics -= 1 if i == problem else 0
 
-                if not is_every_problem_solved_by_at_least_one:
-                    is_every_problem_solved_by_at_least_one = True
-                    cnt_every_problem_solved_by_at_least_one += 1
-
-    score = (1 if nobody_solved_all else 0) + \
-        (1 if cnt_every_problem_solved_by_at_least_one == problem else 0) + \
-        (1 if no_problem_solved_by_everyone else 0) + \
-        (1 if everyone_solved_atleast_one else 0)
-
-    print(score)
-
-
-# while True:
-#     n, m = map(int, input().split())
-#     if n == m == 0:
-#         break
-#     carac = 4
-#     competitor = []
-#     for i in range(n):
-#         competitor.append([int(x) for x in input().split()])
-
-#     for i in range(n):
-#         p = competitor[i].count(1)
-#         if p == m:
-#             carac -= 1
-#             break
-
-#     problema = [0] * m
-#     for i in range(n):
-#         for j in range(m):
-#             if competitor[i][j] == 1:
-#                 problema[j] += 1
-#     if problema.count(0) > 0:
-#         carac -= 1
-
-#     for j in range(m):
-#         if problema[j] == n:
-#             carac -= 1
-#             break
-
-#     for i in range(n):
-#         p = competitor[i].count(0)
-#         if p == m:
-#             carac -= 1
-#             break
-
-#     print(carac)
+    print(characteristics, '\n', problemsAnswered)
