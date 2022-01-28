bf52a4efc247e300b39d081ced837d75a0a5ffc1
Muhammad Tahsin Amin <muhammadtahsinamin@gmail.com>
2022-01-28 18:56:53 +0600

    01.28

--- src/my-project/uri.py
+++ src/my-project/uri.py
@@ -9,16 +9,22 @@ while True:
 
     for i in range(everyone):
         arr.append([int(j) for j in input().split()])
-        characteristics -= (1 if (arr[i].count(1) == 0) else 0)
-        characteristics -= (1 if (arr[i].count(0) == 0) else 0)
+
+        characteristics -= 1 if (arr[i].count(1) == problem) else 0  # 1
+        characteristics -= 1 if (arr[i].count(1) < 1) else 0  # 4
 
     problemsAnswered = [0]*problem
     for i in range(problem):
         for j in range(everyone):
-            problemsAnswered[j] += 1 if arr[j][i] == 1 else 0
+            problemsAnswered[i] += 1 if arr[j][i] == 1 else 0
 
-    for i in problemsAnswered:
-        characteristics -= 1 if i == 0 else 0
-        characteristics -= 1 if i == problem else 0
+    characteristics -= 1 if 0 in problemsAnswered else 0  # 2
+    characteristics -= 1 if everyone in problemsAnswered else 0  # 3
 
     print(characteristics, '\n', problemsAnswered)
+
+
+# 3 3
+# 1 1 1
+# 1 1 1
+# 1 1 1
